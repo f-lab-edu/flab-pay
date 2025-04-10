57a2e3715892e4d6adb3fc7170d1c26358a61af8
yoeubi <yoeubi28@gmail.com>
2025-04-10 22:43:40 +0900

    init

---
+++ src/main/java/org/flab/flab/FlabApplication.java
@@ -0,0 +1,13 @@
+package org.flab.flab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/flab/flab/FlabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.flab.flab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
